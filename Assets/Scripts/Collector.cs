0ebab1fb1dc4f7725cdc2e2472cd1253962cec05
Andrew Huang <andh@umich.edu>
2018-01-11 15:28:28 -0500

    tile walls and rupees

---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collector : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		GameObject other_object = other.gameObject;
+
+		if (other_object.tag == "rupee")
+		{
+			Debug.Log("Rupee collected!");
+			Destroy(other_object);
+		}
+	}
+}
