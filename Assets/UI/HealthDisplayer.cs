cc690a57a2f5831447a9235bcee3c43c03bfdb39
anuticciwb <anuticciwb@gmail.com>
2018-01-11 20:30:36 -0500

    health ui

---
+++ Assets/UI/HealthDisplayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplayer : MonoBehaviour {
+
+	public Inventory inventory;
+	Text textComponent;
+
+	// Use this for initialization
+	void Start () {
+		textComponent = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (inventory != null && textComponent != null)
+		{
+			textComponent.text = "Health: " + 
+				inventory.GetHealth().ToString();
+		}
+	}
+}
