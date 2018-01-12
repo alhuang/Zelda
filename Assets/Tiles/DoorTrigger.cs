c3339ba10e07ab1b67949f43587b9aa59690ebea
anuticciwb <anuticciwb@gmail.com>
2018-01-12 14:49:13 -0500

    camera pan

--- Assets/Tiles/DoorTrigger.cs
+++ Assets/Tiles/DoorTrigger.cs
@@ -6,6 +6,7 @@ public class DoorTrigger : MonoBehaviour {
 
 	public GameObject link;
 	public GameObject camera;
+	public float panTime = 10f;
 
 	private static float CAMERA_LR_MOVE = 16f;
 	private static float CAMERA_UD_MOVE = 11f;
@@ -20,45 +21,57 @@ public class DoorTrigger : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		ArrowKeyMovement arrowKey = other.GetComponent<ArrowKeyMovement> ();
 		if (arrowKey != null) { //link, not monster
-			string direction = arrowKey.GetDirection();
+			StartCoroutine(moveLinkAndCamera(arrowKey));
+		}
+	}
 
-			float cameraLRAmount = CAMERA_LR_MOVE;
-			float cameraUDAmount = CAMERA_UD_MOVE;
-			float linkLRAmount = LINK_LR_MOVE;
-			float linkUDAmount = LINK_UD_MOVE;
+	IEnumerator moveLinkAndCamera(ArrowKeyMovement arrowKey) {
+		string direction = arrowKey.GetDirection();
 
-			//get correct movement amount
-			if (direction == "South") {
-				cameraLRAmount = 0f;
-				cameraUDAmount *= -1f;
+		float cameraLRAmount = CAMERA_LR_MOVE;
+		float cameraUDAmount = CAMERA_UD_MOVE;
+		float linkLRAmount = LINK_LR_MOVE;
+		float linkUDAmount = LINK_UD_MOVE;
 
-				linkLRAmount = 0f;
-				linkUDAmount *= -1;
-			} else if (direction == "North") {
-				cameraLRAmount = 0f;
+		//get correct movement amount
+		if (direction == "South") {
+			cameraLRAmount = 0f;
+			cameraUDAmount *= -1f;
 
-				linkLRAmount = 0f;
-			} else if (direction == "West") {
-				cameraLRAmount *= -1f;
-				cameraUDAmount = 0f;
+			linkLRAmount = 0f;
+			linkUDAmount *= -1;
+		} else if (direction == "North") {
+			cameraLRAmount = 0f;
 
-				linkLRAmount *= -1f;
-				linkUDAmount = 0f;
-			} else {
-				cameraUDAmount = 0f;
+			linkLRAmount = 0f;
+		} else if (direction == "West") {
+			cameraLRAmount *= -1f;
+			cameraUDAmount = 0f;
 
-				linkUDAmount = 0f;
-			}
+			linkLRAmount *= -1f;
+			linkUDAmount = 0f;
+		} else {
+			cameraUDAmount = 0f;
 
-			link.transform.position = new Vector3 (
-				link.transform.position.x + linkLRAmount,
-				link.transform.position.y + linkUDAmount,
-				link.transform.position.z);
+			linkUDAmount = 0f;
+		}
 
+		Vector3 linkNewPosition = new Vector3 (
+			link.transform.position.x + linkLRAmount,
+			link.transform.position.y + linkUDAmount,
+			link.transform.position.z);
+
+		link.transform.position = linkNewPosition;
+
+
+		cameraLRAmount /= panTime;
+		cameraUDAmount /= panTime;
+		for (int i = 0; i < panTime; i++) {
 			camera.transform.position = new Vector3 (
 				camera.transform.position.x + cameraLRAmount,
 				camera.transform.position.y + cameraUDAmount,
 				camera.transform.position.z);
+			yield return new WaitForSeconds (1 / panTime);
 		}
 	}
 }
