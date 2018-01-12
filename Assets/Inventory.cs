564edc7b1b14de7a87461ad1feaf52220ababb9e
anuticciwb <anuticciwb@gmail.com>
2018-01-11 20:19:32 -0500

    add health inventory functions

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Inventory : MonoBehaviour {
 
 	int rupee_count = 0;
+	float health_count = 3f;
+
+	private float max_health = 3f;
 
 	public void AddRupees(int num_rupees)
 	{
@@ -16,5 +19,20 @@ public class Inventory : MonoBehaviour {
 		return rupee_count;
 	}
 
+	public void SubtractHealth(float num_health) {
+		health_count = Mathf.Max (0f, health_count - num_health);
+	}
+
+	public void AddHealth(float num_health) {
+		health_count = Mathf.Min (max_health, health_count + num_health);
+	}
+
+	public int GetHealth() {
+		return health_count;
+	}
+
+	public void AddMaxHealth(float num_health) {
+		max_health += num_health;
+	}
 	
 }
