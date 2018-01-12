bbc54e37d4fbe6b9b3041a7d7ed613990bfe7b0a
anuticciwb <anuticciwb@gmail.com>
2018-01-12 17:17:08 -0500

    change back

--- Assets/Enemies/EnemyDamage.cs
+++ Assets/Enemies/EnemyDamage.cs
@@ -16,12 +16,11 @@ public class EnemyDamage : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		Health linkHealth = other.GetComponent<Health> ();
+		if (other.tag == "Link") {
+			Health linkHealth = other.GetComponent<Health> ();
 
-		if (linkHealth == null) {
-			return;
+			linkHealth.SubtractHealth (damageAmount);
 		}
-		linkHealth.SubtractHealth (damageAmount);
 	}
 
 }
