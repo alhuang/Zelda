84f1eca7c677c5cf85838681f4c8a869a43f6fb1
anuticciwb <anuticciwb@gmail.com>
2018-01-12 16:50:45 -0500

    Enemy Damage

---
+++ Assets/Enemies/EnemyDamage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+	public float damageAmount = .5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Link") {
+			Health linkHealth = other.GetComponent<Health> ();
+
+			linkHealth.SubtractHealth (damageAmount);
+		}
+	}
+}
