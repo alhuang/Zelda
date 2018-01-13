d5c475df79cba6762e08d97040bde88e77e55471
anuticciwb <anuticciwb@gmail.com>
2018-01-13 01:15:23 -0500

    locked doors

---
+++ Assets/Tiles/UnlockLR.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockLR : MonoBehaviour {
+	public GameObject door;
+
+	public Sprite sprite;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Link") {
+			Inventory linkventory = other.GetComponent<Inventory> ();
+			if (linkventory.GetKeys () < 1) {
+				return;
+			}
+
+			//use a key, change the tiles of the door, delete self
+			linkventory.RemoveKey();
+
+			door.GetComponent<SpriteRenderer> ().sprite = sprite;
+			door.GetComponent<BoxCollider> ().enabled = false;
+
+			Destroy (gameObject);
+
+		}
+	}
+}
---
+++ Assets/Tiles/UnlockUD.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockUD : MonoBehaviour {
+	public GameObject LeftDoor;
+	public GameObject RightDoor;
+
+	public Sprite LDSprite;
+	public Sprite RDSprite;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Link") {
+			Inventory linkventory = other.GetComponent<Inventory> ();
+			if (linkventory.GetKeys () < 1) {
+				return;
+			}
+
+			//use a key, change the tiles of the door, delete self
+			linkventory.RemoveKey();
+
+			LeftDoor.GetComponent<SpriteRenderer> ().sprite = LDSprite;
+			RightDoor.GetComponent<SpriteRenderer> ().sprite = RDSprite;
+			LeftDoor.GetComponent<BoxCollider> ().enabled = false;
+			RightDoor.GetComponent<BoxCollider> ().enabled = false;
+
+			Destroy (gameObject);
+
+		}
+	}
+}
