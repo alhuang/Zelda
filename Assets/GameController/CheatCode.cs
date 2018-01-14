6cc402023d35f94b42d7bde2008a991ada7d847a
Andrew Huang <andh@umich.edu>
2018-01-14 17:29:54 -0500

    fixed sprites and ui and camera

--- Assets/GameController/CheatCode.cs
+++ Assets/GameController/CheatCode.cs
@@ -16,7 +16,7 @@ public class CheatCode : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.P)) {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
 			cheat = true;
 		}
 
