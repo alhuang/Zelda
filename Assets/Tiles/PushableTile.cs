d42475dabac41a64b6634eb338f8b54d26f9254a
anuticciwb <anuticciwb@gmail.com>
2018-01-18 23:40:22 -0500

    pushable door + unlock script

--- Assets/Tiles/PushableTile.cs
+++ Assets/Tiles/PushableTile.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PushableTile : MonoBehaviour {
+	public Vector3 directionToPush;
+	public float pushAmount = .05f;
+	public GameObject unlockDoor;
+	public Sprite newSprite;
+
+	private Vector3 finalLocation;
+	private BoxCollider triggerBox;
 
 	// Use this for initialization
 	void Start () {
-		
+		finalLocation = transform.position + directionToPush;
+
+		BoxCollider[] boxes = GetComponents<BoxCollider> ();
+		foreach(BoxCollider box in boxes) {
+			if (box.isTrigger) {
+				triggerBox = box;
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
 		
+	void OnTriggerStay (Collider other) {
+		if (other.tag == "Link") {
+			this.transform.position += directionToPush * pushAmount;
+
+			//if pushed one block, then remove collider
+			//and unlock door
+			if (Mathf.Abs (transform.position.x - finalLocation.x) < pushAmount &&
+				Mathf.Abs (transform.position.y - finalLocation.y) < pushAmount) {
+				triggerBox.enabled = false;
+
+				unlockDoor.GetComponent<SpriteRenderer> ().sprite = newSprite;
+				unlockDoor.GetComponent<BoxCollider> ().enabled = false;
+			}
+		}
 	}
 }
