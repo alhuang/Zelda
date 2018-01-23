51b3285d16abc39ab17e7498af02dfba5f1fef18
anuticciwb <anuticciwb@gmail.com>
2018-01-23 18:06:05 -0500

    spawn animation

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour {
 	private Enemy_Movement[] enemies;
+	private Transform[] enemyPos;
+	private SpriteRenderer[] enemySprites;
 	private float[] speeds;
+	private Vector2[] locations;
+	private Sprite[] sprites;
+
+	public Sprite bigCloud;
+	public Sprite middleCloud;
+	public Sprite littleCloud;
 
 	void Start() {
 		enemies = GetComponentsInChildren<Enemy_Movement> ();
-		speeds = new float[2];
+		enemyPos = GetComponentsInChildren<Transform> ();
+		enemySprites = GetComponentsInChildren<SpriteRenderer> ();
+		speeds = new float[enemies.Length];
+		locations = new Vector2[enemies.Length];
+		sprites = new Sprite[enemies.Length];
 		for (int i = 0; i < enemies.Length; i++) {
 			speeds [i] = enemies [i].GetMovementSpeed();
+			locations [i] = enemyPos [i].position;
+			sprites [i] = enemySprites [i].sprite;
 			enemies [i].movement_speed = 0f;
 		}
 	}
@@ -20,10 +34,27 @@ public class Spawner : MonoBehaviour {
 		if (other.tag == "Link") {
 			for (int i = 0; i < enemies.Length; i++) {
 				if (enemies [i] != null) {
-					enemies [i].movement_speed = speeds [i];
+					enemyPos [i].position = locations [i];
+					StartCoroutine (SpawnEnemies(enemies[i], enemySprites[i],
+										sprites[i], speeds[i]));
 				}
 			}
 		}
+	}
+
+	IEnumerator SpawnEnemies(Enemy_Movement enemy, SpriteRenderer spriteRenderer,
+								Sprite sprite, float speed) {
+		spriteRenderer.sprite = null;
+		yield return new WaitForSeconds (1.5f);
+		spriteRenderer.sprite = bigCloud; 
+		yield return new WaitForSeconds (.75f);
+		spriteRenderer.sprite = middleCloud; 
+		yield return new WaitForSeconds (.1f);
+		spriteRenderer.sprite = littleCloud; 
+		yield return new WaitForSeconds (.1f);
+		spriteRenderer.sprite = sprite; 
+
+		enemy.movement_speed = speed;
 	}
 
 	//activate all enemies
