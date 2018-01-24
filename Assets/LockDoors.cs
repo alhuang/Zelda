134055334d37e755e4f9bb7e8942e2418c8e585e
anuticciwb <anuticciwb@gmail.com>
2018-01-24 00:19:49 -0500

    locking door

---
+++ Assets/LockDoors.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockDoors : MonoBehaviour {
+	public Sprite newSprite;
+	public GameObject door;
+
+	private Sprite oldSprite;
+
+	void Start() {
+		oldSprite = door.GetComponent<SpriteRenderer> ().sprite;
+	}
+
+	void Update() {
+		Health[] ts = GetComponentsInChildren<Health>();
+		if (ts.Length == 0)
+		{
+			door.GetComponent<BoxCollider> ().center = new Vector3(0, .25f, 0);
+			door.GetComponent<BoxCollider> ().size = new Vector3(1, .5f, 1);
+			door.GetComponent<SpriteRenderer> ().sprite = oldSprite;
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Link") {
+			Debug.Log ("do stuff");
+			other.transform.position += new Vector3 (-1f, 0, 0);
+			door.GetComponent<SpriteRenderer> ().sprite = newSprite;
+			door.GetComponent<BoxCollider> ().center = Vector3.zero;
+			door.GetComponent<BoxCollider> ().size = new Vector3(1, 1, 1);
+		}
+
+	}
+}
