6cdd4adb67ba781a0b0a87ba456d877929cab85e
anuticciwb <anuticciwb@gmail.com>
2018-01-23 21:15:42 -0500

    boomerang collectable

---
+++ Assets/Player/AddBoomerang.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBoomerang : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Link") {
+			other.GetComponent<Attack> ().hasBoomerang = true;
+			Destroy (gameObject);
+		}
+	}
+}
