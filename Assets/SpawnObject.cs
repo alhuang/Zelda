d4e25fa8c5ca1aac4c4e6e851541cc1a0cd69216
Andrew Huang <andh@umich.edu>
2018-01-23 22:02:00 -0500

    alpha

---
+++ Assets/SpawnObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+	public GameObject obj;
+	private bool objSpawned = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform[] ts = GetComponentsInChildren<Transform>();
+		if (ts.Length == 0 && !objSpawned)
+		{
+			Debug.Log("Spawned key");
+			Instantiate(obj, this.transform.position, Quaternion.identity);
+			objSpawned = true;
+		}
+	}
+}
