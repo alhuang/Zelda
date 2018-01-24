c3b7b9f5a0d07de6471c15f522398a66b34d403c
anuticciwb <anuticciwb@gmail.com>
2018-01-23 19:56:45 -0500

    spawner

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -3,47 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour {
-	private Enemy_Movement[] enemies;
 	private Transform[] enemyPos;
 	private SpriteRenderer[] enemySprites;
-	private float[] speeds;
 	private Vector2[] locations;
 	private Sprite[] sprites;
+	private bool setLocBool;
 
 	public Sprite bigCloud;
 	public Sprite middleCloud;
 	public Sprite littleCloud;
 
 	void Start() {
-		enemies = GetComponentsInChildren<Enemy_Movement> ();
-		enemyPos = GetComponentsInChildren<Transform> ();
+		Transform[] enemyPos2 = GetComponentsInChildren<Transform> ();
+		enemyPos = new Transform[enemyPos2.Length - 1];
+		int j = 0;
+		foreach (Transform trans in enemyPos2) {
+			if (trans != GetComponent<Transform>()) {
+				enemyPos [j] = trans;
+				j++;
+			}
+		}
+
 		enemySprites = GetComponentsInChildren<SpriteRenderer> ();
-		speeds = new float[enemies.Length];
-		locations = new Vector2[enemies.Length];
-		sprites = new Sprite[enemies.Length];
-		for (int i = 0; i < enemies.Length; i++) {
-			speeds [i] = enemies [i].GetMovementSpeed();
+		locations = new Vector2[enemyPos.Length];
+		sprites = new Sprite[enemyPos.Length];
+		for (int i = 0; i < enemyPos.Length; i++) {
 			locations [i] = enemyPos [i].position;
+			Debug.Log (enemyPos.Length);
 			sprites [i] = enemySprites [i].sprite;
-			enemies [i].movement_speed = 0f;
 		}
 	}
 
 	//activate all enemies
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Link") {
-			for (int i = 0; i < enemies.Length; i++) {
-				if (enemies [i] != null) {
-					enemyPos [i].position = locations [i];
-					StartCoroutine (SpawnEnemies(enemies[i], enemySprites[i],
-										sprites[i], speeds[i]));
+			for (int i = 0; i < enemyPos.Length; i++) {
+				if (enemyPos [i] != null) {
+					//enemyPos [i].position = locations [i];
+					StartCoroutine (SpawnEnemies(enemySprites[i], sprites[i]));
+					StartCoroutine (SetLocations(enemyPos[i], locations[i]));
 				}
 			}
 		}
 	}
 
-	IEnumerator SpawnEnemies(Enemy_Movement enemy, SpriteRenderer spriteRenderer,
-								Sprite sprite, float speed) {
+	//spaghetti code
+	IEnumerator SetLocations(Transform trans, Vector3 Loc) {
+		setLocBool = true;
+		while (setLocBool) {
+			trans.position = Loc;
+			yield return null;
+		}
+	}
+
+	IEnumerator SpawnEnemies(SpriteRenderer spriteRenderer,
+								Sprite sprite) {
 		spriteRenderer.sprite = null;
 		yield return new WaitForSeconds (1.5f);
 		spriteRenderer.sprite = bigCloud; 
@@ -54,18 +68,8 @@ public class Spawner : MonoBehaviour {
 		yield return new WaitForSeconds (.1f);
 		spriteRenderer.sprite = sprite; 
 
-		enemy.movement_speed = speed;
+		setLocBool = false;
 	}
 
-	//activate all enemies
-	void OnTriggerExit(Collider other) {
-		if (other.tag == "Link") {
-			for (int i = 0; i < enemies.Length; i++) {
-				if (enemies [i] != null) {
-					enemies [i].movement_speed = 0f;
-				}
-			}
-		}
-	}
 
 }
