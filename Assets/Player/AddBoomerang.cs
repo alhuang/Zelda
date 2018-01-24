f82b3f8bfeb1712e3772fc548b13b091e9d67efd
anuticciwb <anuticciwb@gmail.com>
2018-01-24 17:13:40 -0500

    test

--- Assets/Player/AddBoomerang.cs
+++ Assets/Player/AddBoomerang.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AddBoomerang : MonoBehaviour {
+	public AudioClip BoomSound;
+
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Link") {
 			other.GetComponent<Attack> ().hasBoomerang = true;
+			AudioSource.PlayClipAtPoint(BoomSound, Camera.main.transform.position);
 			Destroy (gameObject);
 		}
 	}
