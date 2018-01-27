3d2d24d2fc033dc4c83c60c36d451151d767bc52
anuticciwb <anuticciwb@gmail.com>
2018-01-27 16:02:16 -0500

    swapper

---
+++ Assets/Swapper.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swapper : MonoBehaviour {
+	public GameObject Link;
+
+	private bool collided = false;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		if (screenPosition.y > Screen.height || screenPosition.y < 0 || screenPosition.x > Screen.width || screenPosition.x < 0)
+		{
+			//attack.SetCanSpawnSwordProjectile(true);
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("onTriggerEnter");
+		if (other.gameObject.tag == "enemy")
+		{
+			if (collided) {
+				return;
+			}
+			collided = true;
+
+			SwapLocations (other);
+			Health enemy_hp = other.GetComponent<Health>();
+			enemy_hp.SubtractHealth(1f);
+			Debug.Log(enemy_hp.GetHealth());
+			//attack.SetCanSpawnSwordProjectile(true);
+			Destroy(gameObject);
+		}
+
+		//if (other.gameObject.tag != "Link" && other.gameObject.tag != "rupee" && other.gameObject.tag != "heart")
+		//	Destroy(gameObject);
+	}
+
+	void SwapLocations(Collider other) {
+		Debug.Log ("this ran");
+		Vector3 otherPos = other.transform.position;
+		Vector3 linkPos = Link.transform.position;
+		other.transform.position = Vector3.zero;
+		Link.transform.position = otherPos;
+		other.transform.position = linkPos;
+	}
+}
---
+++ Assets/SwapperAttack.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwapperAttack : MonoBehaviour {
+
+	public GameObject arrow;
+	public float arrowSpeed = 5;
+	//Bow, Boomerang, Bomb
+
+	private ArrowKeyMovement arrowKeyMovement;
+	private Health health;
+	private InputToAnimator animator;
+	private string direction_facing = "South";
+	private bool canSpawnBattack = true;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		arrowKeyMovement = GetComponent<ArrowKeyMovement>();
+		health = GetComponent<Health>();
+		animator = GetComponent<InputToAnimator>();
+		//boomerang = GetComponentInChildren<Boomerang>().gameObject;
+		//boomerang.SetActive(false);
+		anim = GetComponent<Animator>();
+		//BWeapon = "";
+	}
+
+	// Update is called once per frame
+	void Update () {
+		direction_facing = arrowKeyMovement.GetDirection();
+		//Debug.Log(canSpawnSwordProjectile);
+
+
+		if (Input.GetKeyDown(KeyCode.Z) && canSpawnBattack)
+		{
+			StartCoroutine("spawnArrow");
+		}
+	}
+
+
+
+	IEnumerator spawnArrow()
+	{
+		Debug.Log ("arrowspawned");
+		GameObject newArrow= null;
+		canSpawnBattack = false;
+		if (direction_facing == "South")
+		{
+			newArrow = (GameObject)Instantiate(arrow, new Vector3(this.transform.position.x, this.transform.position.y - .75f), Quaternion.Euler(0f, 0f, 180f));
+			newArrow.GetComponent<Rigidbody>().velocity = new Vector2(0f, -1f) * arrowSpeed;
+		}
+		else if (direction_facing == "North")
+		{
+			newArrow = (GameObject)Instantiate(arrow, new Vector3(this.transform.position.x, this.transform.position.y + .75f), Quaternion.Euler(0f, 0f, 0f));
+			newArrow.GetComponent<Rigidbody>().velocity = new Vector2(0f, 1f) * arrowSpeed;
+		}
+		else if (direction_facing == "East")
+		{
+			newArrow = (GameObject)Instantiate(arrow, new Vector3(this.transform.position.x + .75f, this.transform.position.y), Quaternion.Euler(0f, 0f, 270f));
+			newArrow.GetComponent<Rigidbody>().velocity = new Vector2(1f, 0f) * arrowSpeed;
+		}
+		else if (direction_facing == "West")
+		{
+			newArrow = (GameObject)Instantiate(arrow, new Vector3(this.transform.position.x - .75f, this.transform.position.y), Quaternion.Euler(0f, 0f, 90f));
+			newArrow.GetComponent<Rigidbody>().velocity = new Vector2(-1f, 0f) * arrowSpeed;
+		}
+		newArrow.GetComponent<Swapper> ().Link = gameObject;
+		yield return new WaitForSeconds(1f);
+		canSpawnBattack = true;
+	}
+
+
+	public void SetCanSpawnBAttack(bool change)
+	{
+		canSpawnBattack = change;
+	}
+}
