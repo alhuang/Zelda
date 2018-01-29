e02ba2aaa80a686c4eb82265b659f888515dd3fb
anuticciwb <anuticciwb@gmail.com>
2018-01-28 19:24:24 -0500

    pushable fix

---
+++ Assets/EnablePushable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablePushable : MonoBehaviour {
+	public GameObject obj = null;
+	public Vector2 spawnPosition;
+	public AudioClip discovery = null;
+	private bool objEnabled = false;
+	// Use this for initialization
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Link")
+		{
+			Health[] ts = GetComponentsInChildren<Health>();
+			if (ts.Length == 0 && !objEnabled)
+			{
+				obj.GetComponent<PushableTile> ().pushed = false;
+				objEnabled = true;
+			}
+		}
+
+	}
+}
