0ce50cb2f371ff373e1bb05736828ac076b553a2
anuticciwb <anuticciwb@gmail.com>
2018-01-28 20:20:26 -0500

    zelda added

---
+++ Assets/WinGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinGame : MonoBehaviour {
+	public GameObject WinGameUI;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Link") {
+			WinGameUI.SetActive(true);
+		}
+	}
+}
